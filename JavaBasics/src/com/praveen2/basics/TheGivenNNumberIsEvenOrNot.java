81bcb98357a236451c655f41ea21b8e53e7fa2f5
praveenkumarpantagani <praveen.pantagani18@gmail.com>
2023-02-15 11:45:04 +0530

    Added even or not program

---
+++ JavaBasics/src/com/praveen2/basics/TheGivenNNumberIsEvenOrNot.java
@@ -0,0 +1,13 @@
+package com.praveen2.basics;
+
+public class TheGivenNNumberIsEvenOrNot {
+    public static void main(String[] args) {
+        int number = 20;
+
+        if(number % 2 == 0){
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
+    }
+}
